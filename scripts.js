986aa0124abebdcc8daef3c0bfbbc525ea4ea9ad
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-05 15:07:01 -0500

    Adding instructions for landing button

--- scripts.js
+++ scripts.js
@@ -4,16 +4,30 @@ window.addEventListener("load", function() {
     let takeOff = document.getElementById("takeoff");
     takeOff.addEventListener("click", function(event) {
         if (window.confirm("Confirm that the shuttle is ready for takeoff")) {
-            // Change the status on the screen
+            // Change flight status on the screen
             document.getElementById("flightStatus").innerHTML = "Shuttle in flight";
 
-            // Change the background color of shuttle flight screen
+            // Change the background color of shuttle flight screen to blue
             document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+            
             // Change the height of the shuttle
             let theHeight = document.getElementById("spaceShuttleHeight").innerHTML;
             theNewHeight = Number(theHeight) + 10000;
             document.getElementById("spaceShuttleHeight").innerHTML = String(theNewHeight);
         };
-        console.log("Take Off clicked");
+    })
+
+    let land = document.getElementById("landing");
+    landing.addEventListener("click", function(event) {
+        window.alert("The shuttle is landing.  Landing gear engaged.")
+
+        // Change flight status on the screen
+        document.getElementById("flightStatus").innerHTML = "The shuttle has landed";
+
+        // Change the background color of shuttle flight screen to green
+        document.getElementById("shuttleBackground").style.backgroundColor = 'green';
+
+        // Change the height of the shuttle
+        document.getElementById("spaceShuttleHeight").innerHTML = String(0);
     })
 })
