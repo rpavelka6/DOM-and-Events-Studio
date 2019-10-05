15fe173d10dd6830c50bdd6c54111f008d659bdb
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-05 14:08:24 -0500

    added event listener for take off button

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    let takeOff = document.getElementById("takeoff");
+    takeOff.addEventListener("click", function(event) {
+        console.log("Take Off clicked");
+    })
+})
