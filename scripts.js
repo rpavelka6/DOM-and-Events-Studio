0239e3b4a0eae36981020e1f4c2d490d3a5b1d46
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-05 14:56:12 -0500

    added updated height of shuttle

--- scripts.js
+++ scripts.js
@@ -4,8 +4,15 @@ window.addEventListener("load", function() {
     let takeOff = document.getElementById("takeoff");
     takeOff.addEventListener("click", function(event) {
         if (window.confirm("Confirm that the shuttle is ready for takeoff")) {
+            // Change the status on the screen
             document.getElementById("flightStatus").innerHTML = "Shuttle in flight";
-            shuttleBackground.style.backgroundColor = "blue";
+
+            // Change the background color of shuttle flight screen
+            document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+            // Change the height of the shuttle
+            let theHeight = document.getElementById("spaceShuttleHeight").innerHTML;
+            theNewHeight = Number(theHeight) + 10000;
+            document.getElementById("spaceShuttleHeight").innerHTML = String(theNewHeight);
         };
         console.log("Take Off clicked");
     })
