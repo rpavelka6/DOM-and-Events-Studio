2ea5a05b1928914e8bc9bd5a4ffe8973bd2d5da4
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-05 14:23:31 -0500

    changed background color of flight screen

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@
 window.addEventListener("load", function() {
     let takeOff = document.getElementById("takeoff");
     takeOff.addEventListener("click", function(event) {
+        if (window.confirm("Confirm that the shuttle is ready for takeoff")) {
+            flightStatus = 'Shuttle in flight';
+            shuttleBackground.style.backgroundColor = "blue";
+        };
         console.log("Take Off clicked");
     })
 })
